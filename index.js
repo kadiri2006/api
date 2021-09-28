3b9f94d9ac44f726a0b9213a55f009ade2025fdd
kadiri2006 <81483190+kadiri2006@users.noreply.github.com>
2021-09-28 15:04:53 +0530

    added cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const dotenv =require("dotenv")
 dotenv.config()
 
 
+
 app.listen(process.env.PORT, () => {
   console.log("server connected ....");
 });
@@ -16,7 +17,20 @@ mongoose.connect(
   process.env.MONGO
 ).then(()=>{console.log("db connected..")}).catch((err)=>console.log(err))
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+
+
+
 app.use(express.json())
+
 
 app.use("/first", router);
 app.use("/sign",credential)
